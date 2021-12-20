fcbc49c341297b7e99d197385d82d484a5fc1c8e
David Omenyo <davidomenyo@gmail.com>
2021-12-20 18:34:54 +0000

    wrote script to process and output sermon data

---
+++ process-sermons.js
@@ -0,0 +1,195 @@
+import { google } from 'googleapis';
+import SpotifyWebApi from "spotify-web-api-node";
+import dateFormat from 'dateformat';
+import capitalizeTitle from 'capitalize-title';
+import {createRequestParam, getDate, getSpeaker, getTimestampParam} from './utils.js'
+import jsf from 'jsonfile';
+import {createObjectCsvWriter} from "csv-writer";
+
+
+//API keys and secrets
+const client_id = '';
+const client_secret = '';
+const api_key = '';
+
+//file paths of data output
+const datafile = 'data.json';
+const csvfile = 'data.csv';
+
+const youtubeBaseURL = 'https://youtube.com/watch?v=';
+
+export var data = {
+  sermons: []
+};
+
+//IDs of public resources
+const sermonPlaylistId = 'PLypLoRrvfFpPGiPdoDeeDA9Q9wzwTM5I8';
+const livestreamPlaylistId = 'PLypLoRrvfFpPT3EdmDrUZ9IlVepySdMuz';
+const gbcPodcastId = '1vaBG0PUEdA75ji6cr74d3';
+
+//accessing the YouTube API
+const youtube = google.youtube({
+  version: 'v3',
+  auth: api_key
+});
+
+const spotify = new SpotifyWebApi ({
+  clientId: client_id,
+  clientSecret: client_secret
+});
+
+
+/**
+ *
+ * @param item
+ */
+function createSermonObject(item) {
+  let titleArray = item.snippet.title.split(' - ');
+  let title = capitalizeTitle(titleArray[0].trim());
+  let scripture = capitalizeTitle(titleArray[1].trim());
+  let description = item.snippet.description;
+  description = description === undefined ? description : description.toLowerCase();
+  let videoId = item.contentDetails.videoId;
+  let desArray = description.split('\n');
+  let date_dirty = getDate(desArray);
+  date_dirty = date_dirty === null ? item.contentDetails.videoPublishedAt : date_dirty;
+  let date = dateFormat(date_dirty, 'dd mmmm yyyy');
+  // let date = dateFormat(date_dirty, 'mmmm dd, yyyy hh:mm pm');
+
+  let sermon = {
+    title: title,
+    speaker: getSpeaker(description),
+    scripture: scripture,
+    date_dirty: date_dirty,
+    date: date,
+    youTubeUrl: youtubeBaseURL + videoId,
+    id: videoId
+  }
+
+  data.sermons.push(sermon);
+}
+
+/**
+ *
+ * @param item
+ */
+function createLivestreamObject(item) {
+  let description = item.snippet.description.toLowerCase();
+  let lines = description.split('\n');
+
+  let title = lines.find(line => line.startsWith('title')).substring('title:'.length);
+  title = capitalizeTitle(title.trim());
+
+  let scripture = lines.find(line => line.includes('scripture')).substring('scripture:'.length);
+  scripture = capitalizeTitle(scripture.trim());
+
+  let videoId = item.contentDetails.videoId;
+  let urlParams = videoId + getTimestampParam(lines);
+
+  let titleArray = item.snippet.title.split(' - ');
+  let date_dirty = getDate(titleArray);
+  let date = dateFormat(getDate(titleArray), 'dd mmmm yyyy');
+  // let date = dateFormat(getDate(titleArray), 'mmmm dd, yyyy hh:mm pm');
+  let sermon = {
+    title: title,
+    speaker: getSpeaker(description),
+    scripture: scripture,
+    date_dirty: date_dirty,
+    date: date,
+    youTubeUrl: youtubeBaseURL + urlParams,
+    id: videoId
+  }
+
+  data.sermons.push(sermon);
+}
+
+/**
+ *
+ * @param requestParam
+ * @returns {Promise<void>}
+ */
+async function processData(requestParam) {
+  let next = true;
+  while (requestParam.pageToken || next) {
+    await youtube.playlistItems.list(requestParam).then(res => {
+      let items = res.data.items;
+      if (items[0].snippet.title.toLowerCase().includes('service')) {
+        items.forEach(createLivestreamObject);
+      } else {
+        items.forEach(createSermonObject);
+      }
+
+      if (res.data.nextPageToken) {
+        requestParam.pageToken = res.data.nextPageToken;
+      } else {
+        delete requestParam.pageToken;
+        next = false;
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+}
+
+let sermonRequestParam = createRequestParam(sermonPlaylistId);
+let livestreamRequestParam = createRequestParam(livestreamPlaylistId);
+
+
+await processData(sermonRequestParam);
+await processData(livestreamRequestParam);
+
+data.sermons.sort((a, b) => {
+  if (new Date(a.date_dirty).getTime() > new Date(b.date_dirty).getTime()) {
+    return -1;
+  } else if (new Date(a.date_dirty).getTime() < new Date(b.date_dirty).getTime()) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+
+for (let i = data.sermons.length, j = 0; i > 0; i--, j++) {
+  data.sermons[j].sortId = i;
+}
+
+await spotify.clientCredentialsGrant().then(res => {
+  spotify.setAccessToken(res.body.access_token);
+  return spotify.getShow(gbcPodcastId, {market: 'ES'});
+}).then(res => {
+  let items = res.body.episodes.items;
+  items.forEach(item => {
+    let title = item.name.split('-', 1)[0].trim();
+    let uri = item.uri;
+    let link = item.external_urls.spotify;
+
+    let index = data.sermons.findIndex(sermon => sermon.title.toLowerCase() === title.toLowerCase());
+    if(index != -1) {
+      // data.sermons[index].spotifyURI = uri;
+      data.sermons[index].spotifyURL = link;
+    }
+  });
+}).catch(err => {
+  console.log(err);
+});
+
+//writing data object to a JSON file.
+jsf.writeFileSync(datafile, data, {spaces: 2});
+
+const csvWriter = createObjectCsvWriter({
+  path: csvfile,
+  header: [
+    {id: 'title', title: 'Title'},
+    {id: 'speaker', title: 'Speaker'},
+    {id: 'scripture', title: 'Scripture'},
+    {id: 'date_dirty', title: 'Date_Dirty'},
+    {id: 'date', title: 'Date'},
+    {id: 'youTubeUrl', title: 'YouTube URL'},
+    {id: 'id', title: 'ID'},
+    {id: 'sortId', title: 'Sort ID'},
+    {id: 'spotifyURL', title: 'Spotify URL'}
+  ]
+});
+
+await csvWriter.writeRecords(data.sermons);
+
+console.log(data.sermons.length);
---
+++ utils.js
@@ -0,0 +1,107 @@
+import capitalizeTitle from "capitalize-title";
+
+let months = new Map();
+
+months.set('January', 0);
+months.set('February', 1);
+months.set('March', 2);
+months.set('April', 3);
+months.set('May', 4);
+months.set('June', 5);
+months.set('July', 6);
+months.set('August', 7);
+months.set('September', 8);
+months.set('October', 9);
+months.set('November', 10);
+months.set('December', 11);
+
+
+//Utility functions
+
+/**
+ * creates the request parameters for the api call.
+ * @param playlistId
+ * @returns {{playlistId, maxResults: number, part: string}}
+ */
+export function createRequestParam(playlistId) {
+    let requestParam = {
+        part: 'contentDetails,snippet',
+        playlistId: playlistId,
+        maxResults: 50
+    }
+    return requestParam;
+}
+
+/**
+ *
+ * @param description
+ * @returns {string}
+ */
+export function getSpeaker(description) {
+    if (description.includes("speaker")) {
+        let desArray = description.split("\n");
+        let speaker = desArray.find(line => line.includes("speaker")).split(':')[1].trim();
+        speaker = capitalizeTitle(speaker);
+        return speaker;
+    }
+}
+
+/**
+ *
+ * @param lines
+ * @returns {string}
+ */
+export function getTimestampParam(lines) {
+    let timestamp = lines.find(line => line.includes("sermon")).split('-')[0].trim();
+    return '&t=' + convertTimestampToSeconds(timestamp) + 's';
+}
+
+/**
+ *
+ * @param timestamp
+ * @returns {number|*}
+ */
+function convertTimestampToSeconds(timestamp) {
+    let time = timestamp.split(':');
+    let hrs = 0;
+    let mins = 0;
+    let seconds = 0;
+    if (time.length === 2) {
+        mins = Number.parseInt(time[0]);
+        seconds = Number.parseInt(time[1]);
+        let result = (60 * mins) + seconds;
+        return result;
+    } else {
+        hrs = time[0];
+        mins = time[1];
+        seconds = time[2];
+        let result = (3600 * hrs) + (60 * mins) + seconds;
+        return result;
+    }
+}
+
+/**
+ *
+ * @param array
+ * @returns {null|Date}
+ */
+export function getDate(array) {
+    if (array.length === 2 && array[0].toLowerCase().includes('service')) {
+        let date = array[1];
+        date = date.trim().split(' ');
+        date[0] = date[0].replace(/\D/g, '');
+        date[1] = months.get(date[1]);
+        date = new Date(date[2], date[1], date[0]);
+        return date;
+    } else {
+        let str = array.find(line => line.toLowerCase().startsWith('date'));
+        if (str != undefined) {
+            let date = array.filter(line => line.includes("date"))[0].split(':')[1].trim();
+            let dateSplit = date.split('/');
+            date = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
+            return date;
+        } else {
+            return null;
+        }
+    }
+}
